852d0946f20c4ea274dfadcd864acd83b23e3771
TT-1024 <ttye03094@gmail.com>
2026-08-17 17:26:32 +0800

    fix: force Safari cache migration

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* TT工作台 - Service Worker for offline support */
-const CACHE_NAME = 'tt-workbench-v9-categories';
+const CACHE_NAME = 'tt-workbench-v10-auto-update';
 const ASSETS = [
   './',
   './index.html',
